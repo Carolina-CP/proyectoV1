00063b037212dc630eba01a04a9a0408cba71c10
carola <carolacollao@gmail.com>
2022-10-29 17:03:33 -0300

    estilo tarjeta

--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -8,34 +8,39 @@ import Button from 'react-bootstrap/esm/Button';
 const Tarjeta = () => {
 
     const { data } = useContext(DataContext);
-  
+
     console.log('este es novedades')
     console.log({ data })
-  
-  
+
+
     return (
-     
+
         <div >
-          <div className="row row-cols-md-auto justify-content-center">
-            {data.map(d =>
-              <Card className='m-2' style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={d.img} />
-                <Card.Body>
-                  <Card.Title>{d.nombre}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">{d.proveedor}</Card.Subtitle>
-                  <Card.Text>
-                    {d.descripcion}
-                  </Card.Text>
-                  <Button variant="primary">Más info</Button>
-                </Card.Body>
-              </Card>
-            )}
-          </div>
-  
-  
+            <div className="row row-cols-md-auto justify-content-center">
+                {data.map(d =>
+                    <Card className='m-2' style={{ width: '18rem' }}>
+                        <Card.Title>{d.nombre}</Card.Title>
+                        <Card.Img variant="top" src={d.img} />
+                        <Card.Body>
+
+                            <Card.Subtitle className="mb-2 text-muted">{d.proveedor}</Card.Subtitle>
+                            <div className="row row-cols-md-auto justify-content-between">
+                                <span
+                                    style={{ color: 'rgb(221, 110, 66)' }}
+                                    className='fs-4'
+                                >${d.precio}</span>
+
+                                <Button style={{ backgroundColor: 'rgb(192, 214, 223)', color: 'rgb(79, 109, 122)' }} size="sm">Más info</Button>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                )}
+            </div>
+
+
         </div>
-  
+
     )
-  }
-  
-  export default Tarjeta
+}
+
+export default Tarjeta
--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,15 +1,16 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+
 
 
 export const DataContext = createContext();
-
+/*
 const dataFixed = [{
     categoria: "graficas",
     id: 1,
     nombre: "Diseño de Currículo",
     proveedor: "Juan López",
     img: "https://img.remediosdigitales.com/b929f4/duncan_chapple_horizontal/450_1000.jpg", ingredients: ["mozzarella", "pimientos", "pollo grillé", "orégano"],
-   descripcion: 'Este curso bla bla bla, y mucho mas bla bla bla',
+    descripcion: 'Este curso bla bla bla, y mucho mas bla bla bla',
     favorito: false
 },
 {
@@ -22,11 +23,42 @@ const dataFixed = [{
     favorito: false
 }]
 
-
+*/
 
 export const DataProvider = ({ children }) => {
 
-    const [data, setData] = useState(dataFixed);
+    const [data, setData] = useState([]);
+
+    // useeffect para llamar a la api de servicios    
+    useEffect(() => {
+        consultarInformacion1();
+    }, []);
+
+
+    const consultarInformacion1 = async () => {
+        const endpoint = '../../public/pizzas.json'
+        const respuesta = await fetch(endpoint);
+        const data = await respuesta.json();
+        setData(data)
+    }
+
+    console.log(data)
+
+     
+  useEffect(() => {
+    consultarInformacion();
+}, []);
+
+
+const consultarInformacion = async () => {
+
+    const response = await fetch('./pizzas.json')
+    const data = await response.json()
+    setData(data);
+}
+
+console.log(data)
+
 
     return (
         <DataContext.Provider value={{ data, setData }}>
